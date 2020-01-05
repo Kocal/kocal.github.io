6b7ee344de3d6a7a3b11201514a408201c5a7060
Hugo Alliaume <kocal@live.fr>
2020-01-05 20:29:36 +0100

    configure comments

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -21,12 +21,25 @@ module.exports = {
       { text: 'Twitter', link: 'https://twitter.com/HugoAlliaume' },
     ],
     modifyBlogPluginOptions(blogPluginOptions) {
+      const sitemap = {
+        ...blogPluginOptions.sitemap,
+        hostname: 'https://hugo.alliau.me',
+      };
+
+      const comment = {
+        service: "vssue",
+        autoCreateIssue: true,
+        prefix: "[Post] ",
+        owner: "Kocal",
+        repo: "blog",
+        clientId: "6ac293ccc1174292305a",
+        clientSecret: "93224fc793ab8375fb986a294ba27995d41afde9",
+      };
+
       return {
         ...blogPluginOptions,
-        sitemap: {
-          ...blogPluginOptions.sitemap,
-          hostname: 'https://hugo.alliau.me',
-        },
+        sitemap,
+        comment,
       };
     },
   },
