bf47735aa251ae03edbd892862d11f6d9e697ef3
Hugo Alliaume <kocal@live.fr>
2020-01-05 19:45:06 +0100

    use @vuepress/theme-blog

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -4,35 +4,30 @@ module.exports = {
   plugins: [
     '@vuepress/plugin-nprogress',
     ['@vuepress/medium-zoom', true],
-    [
-      '@vuepress/blog',
-      {
-        directories: [
-          {
-            id: 'post',
-            dirname: '_posts',
-            itemPermalink: '/:year/:month/:day/:slug',
-          },
-        ],
-        sitemap: {
-          hostname: 'https://hugo.alliau.me',
-          exclude: ['/404.html', '/post/']
-        },
-      },
-    ],
     'seo',
     ['social-share', {
       networks: ['twitter', 'facebook', 'reddit'],
       twitterUser: 'HugoAlliaume',
-    }]
+    }],
   ],
+  theme: '@vuepress/blog',
   themeConfig: {
     repo: 'Kocal/blog',
     editLinks: true,
     lastConfig: 'Last updated',
     nav: [
-      { text: 'Tags', link: '/tags/' },
+      { text: 'Tags', link: '/tag/' },
+      { text: 'GitHub', link: 'https://github.com/Kocal' },
       { text: 'Twitter', link: 'https://twitter.com/HugoAlliaume' },
     ],
+    modifyBlogPluginOptions(blogPluginOptions) {
+      return {
+        ...blogPluginOptions,
+        sitemap: {
+          ...blogPluginOptions.sitemap,
+          hostname: 'https://hugo.alliau.me',
+        },
+      };
+    },
   },
 };
