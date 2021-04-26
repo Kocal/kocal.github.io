47e1698dfa446430a0f32a94fc1892f988322184
Hugo Alliaume <hugo@alliau.me>
2021-04-26 12:34:42 +0200

    no more comments integration, just use a dumb link to repo issues...
    I'm so done with SSR shit, I guess

--- .vuepress/theme/layouts/Post.vue
+++ .vuepress/theme/layouts/Post.vue
@@ -22,7 +22,10 @@
         <footer>
           <PageEdit />
           <hr>
-          <Comment />
+          <a :href="commentUrl" class="btn-comment" target="_blank" rel="noopener noreferrer">
+            Leave a comment!
+            <ExternalLinkIcon style="margin-left: 5px"/>
+          </a>
         </footer>
       </article>
     </main>
@@ -34,10 +37,33 @@ import Page from '@theme/components/Page.vue';
 import PageEdit from '@theme/components/PageEdit.vue';
 import PostMeta from '@theme/components/PostMeta.vue';
 import SidebarLinks from '@theme/components/SidebarLinks.vue';
-import { Comment } from '@vuepress/plugin-blog/lib/client/components';
+import {ExternalLinkIcon} from 'vue-feather-icons'
 
 export default {
   name: "Post",
-  components: { PostMeta, Page, PageEdit, SidebarLinks, Comment },
+  components: { PostMeta, Page, PageEdit, SidebarLinks, ExternalLinkIcon },
+  computed: {
+    commentUrl() {
+      return `https://github.com/${this.$themeConfig.repo}/issues?q=${encodeURIComponent('is:issue is:open ' + this.$frontmatter.title)}`;
+    },
+  },
 };
 </script>
+
+<style scoped lang="stylus">
+.btn-comment {
+  display flex;
+  align-items center
+  justify-content center
+  padding: 12px;
+  font-size 20px;
+  background-color $accentColor;
+  color #fff;
+  border-radius: 5px;
+  text-decoration none !important;
+
+  &:hover {
+    background-color: darken($accentColor, 10%);
+  }
+}
+</style>
