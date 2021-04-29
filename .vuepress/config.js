6a20d205c4ca659253aa2ff7c00b58c7838c28e2
Hugo Alliaume <hugo@alliau.me>
2021-04-29 20:15:01 +0200

    feat: migrate to Google Analytics GA4

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -2,15 +2,18 @@ module.exports = {
   title: 'Hugo Alliaume',
   description: 'My personal blog',
   head: [
-    ['link', { rel: "icon", type: "image/png", href: "/favicon.png"}],
-  ],
-  plugins: [
-    [
-      '@vuepress/google-analytics',
-      {
-        'ga': 'UA-93495564-3',
-      },
-    ],
+    ['link', { rel: "icon", type: "image/png", href: "/favicon.png" }],
+    ['script', {
+      async: true,
+      src: 'https://www.googletagmanager.com/gtag/js?id=G-Z8KN175TJZ',
+    }],
+    ['script', {}, [`
+      window.dataLayer = window.dataLayer || [];
+      window.gtag = function(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+    
+      gtag('config', 'G-Z8KN175TJZ');`,
+    ]],
   ],
   themeConfig: {
     repo: 'Kocal/blog',
