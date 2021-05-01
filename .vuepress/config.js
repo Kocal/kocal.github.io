3f4cae31f45972593a18b6eae0ca7647f410f9d3
Hugo Alliaume <hugo@alliau.me>
2021-05-01 11:00:57 +0200

    configure more friendly favicons and fore more platforms

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -2,7 +2,13 @@ module.exports = {
   title: 'Hugo Alliaume',
   description: 'My personal blog',
   head: [
-    ['link', { rel: "icon", type: "image/png", href: "/favicon.png" }],
+    ['link', { rel: "apple-touch-icon", sizes: "180x180", href: "/apple-touch-icon.png" }],
+    ['link', { rel: "icon", type: "image/png", sizes: "32x32", href: "/favicon-32x32.png" }],
+    ['link', { rel: "icon", type: "image/png", sizes: "16x16", href: "/favicon-16x16.png" }],
+    ['link', { rel: "manifest", href: "/site.webmanifest" }],
+    ['link', { rel: "mask-icon", href: "/safari-pinned-tab.svg", color: '#000000' }],
+    ['meta', { name: "msapplication-TileColor", content: "#000000" }],
+    ['meta', { name: "theme-color", content: "#000000" }],
     ['script', {
       async: true,
       src: 'https://www.googletagmanager.com/gtag/js?id=G-Z8KN175TJZ',
@@ -11,7 +17,7 @@ module.exports = {
       window.dataLayer = window.dataLayer || [];
       window.gtag = function(){dataLayer.push(arguments);}
       gtag('js', new Date());
-    
+
       gtag('config', 'G-Z8KN175TJZ');`,
     ]],
   ],
